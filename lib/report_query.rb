e7815f18f0fd84eae9400a3f73ffbe9f584f4097
Ashish Muralidharan <muralidharanashish@gmail.com>
2021-04-10 04:20:54 +0530

    Initial Commit

---
+++ lib/report_query.rb
@@ -0,0 +1,6 @@
+require "report_query/version"
+
+module ReportQuery
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/report_query/version.rb
@@ -0,0 +1,3 @@
+module ReportQuery
+  VERSION = "0.1.0"
+end
